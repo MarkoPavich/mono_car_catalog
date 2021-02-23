3c468d452c87cc9d568033128efd9b1107efe243
Marko <marko.pavic667@gmail.com>
2021-02-23 22:29:16 +0100

    feature-navbar: progress on layout;

--- frontend/src/components/layout/navbar/Navbar.js
+++ frontend/src/components/layout/navbar/Navbar.js
@@ -1,22 +1,34 @@
 import React from 'react'
 import './NavBar.css'
 import logoPNG from './assets/car_shape_silh.png'
+import magnifierPNG from './assets/magnifier_ico.png'
 
 function Navbar() {
     return ( 
         <nav className="l-navbar">
             <div className="l-navbar-content-container-left">
-                <img src={logoPNG} alt="logo"/>
-                <div className="l-navbar-c-left-text-box">
-                    <h1>Mono car catalog</h1>
-                    <span>Powered by V8</span>
+                <div className="l-navbar-logo-container">
+                    <img src={logoPNG} alt="logo"/>
+                    <div className="l-navbar-c-left-text-box">
+                        <h1>Mono car catalog</h1>
+                        <span>Powered by V8</span>  {/* V8 - js reference, get it ? :)) ..I'll, show myself out.. */}
+                    </div>
                 </div>
-                <div className="l-navbar-searchbox-container">
-
+                <div onClick={focusSearchbar} className="l-navbar-searchbox-container">
+                    <div className="l-navbar-searchbar">
+                        <input placeholder="Pretraga" name="nav_search" type="text"/>
+                        <img src={magnifierPNG} alt="search_ico"/>
+                    </div>
                 </div>
             </div>
             <div className="l-navbar-content-container-right">
-                
+                {responsiveUserMenu(true)}
+                <div className="l-navbar-locale-toggle-container">
+                    <select name="navbar_locale_toggle" id="navbar_locale_toggle">
+                        <option value="HR">HR</option>
+                        <option value="EN">EN</option>
+                    </select>
+                </div>
             </div>
         </nav>
     )
@@ -24,3 +36,37 @@ function Navbar() {
 
 export default Navbar;
 
+
+/* Helper fns */
+
+function focusSearchbar(){
+    document.getElementsByName("nav_search")[0].focus();
+}
+
+
+function responsiveUserMenu(smallScreen){
+    const userMenu = (
+        <div className="l-navbar-user-menu">
+            <ul>
+                <li>Add vehicle</li>
+                <li>My vehicles</li>
+                <li>Logout</li>
+            </ul>
+        </div>
+    )
+
+    const userMenuSmallScreen = (
+        <div className="l-navbar-user-menu-mobile">
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+            <div>
+            </div>
+        </div>
+    )
+
+    if(smallScreen) return userMenuSmallScreen;
+    else return userMenu;
+}
